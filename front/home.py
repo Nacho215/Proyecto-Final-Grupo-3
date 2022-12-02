a17b51f3dd89d3db36f5625694894d28820e87cd
Chisaac <chisaacen@gmail.com>
2022-12-02 19:39:03 -0300

    feat: Create modules for frontend with streamlit

---
+++ front/home.py
@@ -0,0 +1,37 @@
+'''Streamlit is launched from this file
+
+'''
+
+# Imports
+import streamlit as st
+
+#Set default configuration for the page
+st.set_page_config(
+    page_title="Bikes forever store",
+    page_icon="🚲",
+    layout="wide",
+)
+
+#Set a container
+body = st.container()
+
+with body:
+    # Welcome message and descriptions
+    st.markdown(
+        """
+        # Welcome to bike 🚲 forever ♾, where all your biking needs will be satisfied. 🙌
+        
+        -------------------------![imagen](https://i.pinimg.com/550x/2b/1c/5f/2b1c5f11939d2faca9c0b2536f7e7c9e.jpg)--------------------------
+        
+        In this webpage you will be able to:
+        # Welcome to bike 🚲 forever ♾, where all your biking needs will be satisfied. 🙌
+        In this webpage you will be able to:
+        * 👉 upload our dataset
+        * 👉 filter the dataset according to your needs
+        * 👉 graph out the most relevant data available.                
+        More information at [Bikes Forever](bikesforever.com.ar)
+       
+        """
+        )
+
+st.text('Fuente: https://www.kaggle.com/datasets/archit9406/customer-transaction-dataset')
